8491c28960d53f902aca292bb7e662895dd4bfff
Demonq2004 <grzegorzekdawid@gmail.com>
2021-03-31 13:18:27 +0200

    Stworzenie polaczenia z bazą danych

---
+++ mysqli.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $dbname = 'dawidgrzegorzek';
+    $port = 3306;
+    
+
+    $polaczenie = @new Mysqli($host,$user,$password,$dbname,$port);
+
+    
+?>
